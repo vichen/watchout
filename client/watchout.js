2034bf21409aa1fc62537ed47deb5ef725bc5d17
vichen <vivian.vc.chen@gmail.com>
2016-03-11 15:54:33 -0800

    get enemies to move

--- client/watchout.js
+++ client/watchout.js
@@ -5,23 +5,40 @@ var gameVars = {
   width: 700,
   height: 450,
   enemyR: 30,
+  numEnemies: 0
 };
 
 var initGame = function(myGameVars) {
   if (myGameVars && !myGameVars.svgContainer) {
-    myGameVars.svgContainer = d3.select('body').append('svg').attr('width', gameVars.width).attr('height', gameVars.height);
-    myGameVars.svgContainer.append('rect').attr('x', 0).attr('y', 0).attr('width', gameVars.width).attr('height', gameVars.height).attr('stroke', 'black').attr('fill', 'none').attr('stroke-width', 'black');
-    myGameVars.svgContainer.append('defs').attr('id', 'imgdefs').append('pattern').attr('id', 'astroidPattern')
-                        .attr('height', 1)
-                        .attr('width', 1)
-                        .attr('x', "0")
-                        .attr('y', "0")
-                        .append("image")
-     .attr("x", -130)
-     .attr("y", -220)
-     .attr("height", 640)
-     .attr("width", 480)
-     .attr("xlink:href", 'asteroid.png');
+
+    myGameVars.svgContainer = 
+      d3.select('body')
+      .append('svg')
+      .attr('width', gameVars.width)
+      .attr('height', gameVars.height);
+
+    myGameVars.svgContainer
+      .append('rect')
+      .attr('x', 0)
+      .attr('y', 0)
+      .attr('width', gameVars.width)
+      .attr('height', gameVars.height)
+      .attr('stroke', 'black')
+      .attr('fill', 'none')
+      .attr('stroke-width', 'black');
+
+    myGameVars.svgContainer
+      .append('svg:pattern')
+      .attr('id', 'asteroidPattern')
+      .attr('width', 1)
+      .attr('height', 1)
+      .attr('patternUnits', 'objectBoundingBox')
+      .append('svg:image')
+      .attr('xlink:href', 'asteroid.png')
+      .attr('width', 60)
+      .attr('height', 60)
+      .attr('x', 0)
+      .attr('y', 0);
   }
 };
 
@@ -29,11 +46,37 @@ var addEnemy = function(myGameVars, num) {
   if (!num) {
     num = 1;
   }
+  myGameVars.numEnemies = num;
   for (var i = 0; i < num; i++) {
     var newX = Math.random() * (myGameVars.width - (myGameVars.enemyR * 2)) + myGameVars.enemyR;
     var newY = Math.random() * (myGameVars.height - (myGameVars.enemyR * 2)) + myGameVars.enemyR;
-    myGameVars.svgContainer.append('circle').attr('cx', newX).attr('cy', newY).attr('r', myGameVars.enemyR).attr('fill', "url(#asteroidPattern)");
+
+    var enemyCircle = 
+      myGameVars.svgContainer
+      .append('circle')
+      .attr('id', 'enemyCircle')
+      .attr('cx', newX)
+      .attr('cy', newY)
+      .attr('r', myGameVars.enemyR)
+      .attr('fill', 'url(#asteroidPattern)');
   }
 };
+
+var moveEnemies = function() {
+  var coordArray = [];
+  for (var i = 0; i < gameVars.numEnemies; i++) {
+    coordArray.push({x: Math.random() * (gameVars.width - (gameVars.enemyR * 2)) + gameVars.enemyR, 
+                     y: Math.random() * (gameVars.height - (gameVars.enemyR * 2)) + gameVars.enemyR});
+  }
+
+  var enemyCircles = d3.select('body').selectAll('circle')
+    .data(coordArray)
+    .transition().duration(1000)
+    .attr('cx', function(d) { return d.x; })
+    .attr('cy', function(d) { return d.y; });
+};
+
+/* init game here */
 initGame(gameVars);
 addEnemy(gameVars, 10);
+setInterval(moveEnemies, 1100);
